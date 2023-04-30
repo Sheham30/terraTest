5fc3f9fc4caae90d50a5ff778546d002bc1f9b2b
sheham30 <shehamhassan@gmail.com>
2023-05-01 04:06:57 +0500

    files added7

--- main.tf
+++ main.tf
@@ -1,9 +1,16 @@
-resource "aws_instance" "this" {
-  ami                     = "ami-02396cdd13e9a1257"
-  instance_type           = "t2.micro"
+resource "aws_instance" "this2" {
+  ami           = "ami-02396cdd13e9a1257"
+  instance_type = "t2.micro"
   tags = {
-    Name        = "MyInstance"
+    Name = "MyInstance"
   }
+
+  user_data = <<-EOF
+              #!/bin/bash
+              # Install Docker
+              sudo apt-get update -y
+              sudo apt-get install -y docker.io
+              # Start Docker service
+              sudo systemctl start docker.service
+              EOF
 }
-
-
