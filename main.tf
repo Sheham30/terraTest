40c9182b88eebf636f91df8700478c4ff94cc411
sheham30 <shehamhassan@gmail.com>
2023-05-01 03:44:16 +0500

    files added6

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
-
-resource "aws_s3_bucket" "example" {
-  bucket = "sheham20203316"
-
+resource "aws_instance" "this" {
+  ami                     = "ami-02396cdd13e9a1257"
+  instance_type           = "t2.micro"
   tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
+    Name        = "MyInstance"
   }
 }
+
+
